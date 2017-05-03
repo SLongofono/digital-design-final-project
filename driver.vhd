46eca733e881eda22f3fcaf63191e581e7bdb553
Stephen Longofono <Onofognol@ku.edu>
2017-05-03 10:00:09 -0500

    Working in final form

--- driver.vhd
+++ driver.vhd
@@ -18,14 +18,12 @@ entity driver is
         blue : out std_logic_vector (3 downto 0);
         green : out std_logic_vector (3 downto 0);
         new_color : in std_logic_vector(11 downto 0);
-        x_write : in integer;
-        y_write : in integer;
         hw_in_up, hw_in_down, hw_in_left, hw_in_right, hw_in_cent : in std_logic;
         SCLK : out std_logic;
         MOSI : out std_logic;
         MISO : in std_logic;
         SS : out std_logic;
-        ledup, leddown, ledleft, ledright : out std_logic
+        activity          : out std_logic
     );
 end driver;
 
@@ -41,8 +39,8 @@ port(
     VGA_BLUE_O : out std_logic_vector (3 downto 0);
     VGA_GREEN_O : out std_logic_vector (3 downto 0);
     new_color : in std_logic_vector(11 downto 0);
-    x_write : in integer;
-    y_write : in integer
+    address : in integer;
+    draw_enable : in std_logic
 );
 end component;
 
@@ -60,13 +58,17 @@ port(	  clk, rst	       : in std_logic;
 	      SS		       : out std_logic);        -- SPI Slave select
 end component;
 
-component button is
-generic(counter_size : INTEGER := 20); -- debounce time control
-port(
-    clk     : in  std_logic;        -- system clock, assumed 100 MHz
-    rst     : in std_logic;         -- system reset
-    hw_in   : in  std_logic;        -- input from physical button
-    button_assert  : out std_logic);--debounced signal
+component controller is
+	PORT(
+		clk               : in std_logic;
+		rst			      : in std_logic;
+		btn_up			  : in std_logic;
+		btn_down		  : in std_logic;
+		btn_left		  : in std_logic;
+		btn_right		  : in std_logic;
+		btn_mode_switch   : in std_logic;
+		addr		      : out integer;
+		en_write	      : out std_logic);
 end component;
 
 constant FRAME_WIDTH : natural := 640;
@@ -79,17 +81,18 @@ signal s_x : integer := 320;
 signal s_y : integer := 240;
 signal s_color : std_logic_vector(11 downto 0);
 
--- Button interface
-signal s_up, s_down, s_left, s_right, s_cent : std_logic;
+-- Button controller interface
+signal draw_enable : std_logic;
+signal s_addr: integer := 153600;
 
 -- Accelerometer interface
 signal shaking : std_logic;
 
--- Speed control
-signal frame_counter : std_logic_vector(20 downto 0) := (others => '0');
-signal frame_clk : std_logic;
+
 
 begin
+
+activity <= draw_enable;
 
 VGA_unit: VGA_ctrl port map(
     clk => clk,
@@ -100,44 +103,20 @@ VGA_unit: VGA_ctrl port map(
     VGA_BLUE_O => blue,
     VGA_GREEN_O => green,
     new_color => new_color,
-    x_write => s_x,
-    y_write => s_y
+    address => s_addr,
+    draw_enable => draw_enable
 );
 
-BTN_UP : button port map(
+Ctrl: controller port map(
     clk => clk,
     rst => rst,
-    hw_in => hw_in_up,
-    button_assert => s_up
-);
-
-BTN_LEFT : button port map(
-    clk => clk,
-    rst => rst,
-    hw_in => hw_in_left,
-    button_assert => s_left
-);
-
-BTN_RIGHT : button port map(
-    clk => clk,
-    rst => rst,
-    hw_in => hw_in_right,
-    button_assert => s_right
-);
-
-BUTTON_CENT : button port map(
-    clk => clk,
-    rst => rst,
-    hw_in => hw_in_cent,
-    button_assert => s_cent
-);
-
-BTN_DOWN : button port map(
-    clk => clk,
-    rst => rst,
-    hw_in => hw_in_down,
-    button_assert => s_down
-);
+    btn_up => hw_in_up,
+    btn_down => hw_in_down,
+    btn_left => hw_in_left,
+    btn_right => hw_in_right,
+    btn_mode_switch => hw_in_cent,
+    addr => s_addr,
+    en_write => draw_enable); -- draw enable
 
 ACC : accel port map(
     clk => clk,
@@ -149,42 +128,5 @@ ACC : accel port map(
     SS => SS
 );
 
--- Human friendly clock for responding to input
-frame_clk <= frame_counter(20);
-
-process(clk, rst)
-begin
-    if('1' = rst) then
-        frame_counter <= (others => '0');
-    elsif(rising_edge(clk)) then
-        frame_counter <= frame_counter + 1;
-    end if;
-end process;
-
--- Handle button inputs
-process(s_up, s_down, s_left, s_right, s_cent, frame_clk)
-begin
-    s_x <= s_x;
-    s_y <= s_y;
-    if(rising_edge(frame_clk)) then
-        
-        if('1' = s_up and s_y < FRAME_HEIGHT) then
-            s_y <= s_y + 1;
-        elsif('1' = s_down and s_y > 0) then
-            s_y <= s_y - 1;
-        elsif('1' = s_right and s_x < FRAME_WIDTH) then
-            s_x <= s_x + 1;
-        elsif('1' = s_left and s_x > 0) then
-            s_x <= s_x - 1;
-        end if;
-        
-    end if;
-
-end process;
-
-ledup <= s_up;
-leddown <= s_down;
-ledleft <= s_left;
-ledright <= s_right;
 
 end Behavioral;
